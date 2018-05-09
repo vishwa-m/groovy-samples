4ea9b0961c01869a49fa1ec8ef47e457d38f8d51
Vishwanath Manchala <vismanch@publicisgroupe.net>
2018-05-09 11:26:15 -0500

    Added Groovy files

---
+++ com/vn/prac/lib/Library.groovy
@@ -0,0 +1,13 @@
+package com.vn.prac.lib
+
+def static sayHello(){
+    println("Hello")
+}
+
+def sayHi(String name){
+    println("Hi " + name)
+}
+
+def static add(int num1, int num2){
+    return num1+num2
+}
---
+++ com/vn/prac/lib/Main.groovy
@@ -0,0 +1,8 @@
+package com.vn.prac.lib
+
+//def externalMethod = load("Library.groovy");
+Library.sayHello()
+println(Library.add(1,3))
+
+int result=Library.add(10,20);
+println(result)
